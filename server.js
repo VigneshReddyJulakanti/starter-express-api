ef2bd30cd1d0352febea6777ba132e002402e31b
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2022-02-03 01:47:04 +0530

    added admin security

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
 const app=express();
-// const mongoURI="mongodb://localhost:27017/testing?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-const mongoURI=process.env.MONGO_ID
+
+const mongoURI=process.env.MONGO_ID || "mongodb://localhost:27017/testing?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 const connectToMongo=async()=>{
  await mongoose.connect(mongoURI,()=>{console.log("connected to mongo")})
 }
@@ -13,8 +13,18 @@ app.use(cors({
   }))
 const port=process.env.PORT || 5000;
 app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send("You are in Home of data base , BOOM :)")
+})
+app.post('/admin',(req,res)=>{
+    if(req.body.user==(process.env.ADMIN_ID|| "boom" )&& req.body.pass==(process.env.ADMIN_PASS || "1426")){
+        
+
+        res.json({success:"true"})
+    }else{
+        res.json({success:"false"})
+    }
 })
 app.use('/api/notes',require('./routes/notes'))
 
